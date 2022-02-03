c44c1a75563d5caa4854c1799de5c1afec0fef5d
jonmanquincy <53950487+jonmanquincy@users.noreply.github.com>
2022-02-03 11:37:25 -0700

    battle manager implemented

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,27 @@
-window.alert("This is an alert! JavaScript is running!");
+import BattleManager from "./battleManager";
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    BattleManager.damageEnemy(playerAttack, enemyHealth);
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    BattleManager.damagePlayer(enemyAttack, playerHealth);
+};
+
+fight();
+
+
