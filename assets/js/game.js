fcbb19cc540dec31c1733b9d8cbe0b8e0e8c4e77
jonmanquincy <53950487+jonmanquincy@users.noreply.github.com>
2022-02-03 11:56:39 -0700

    structure fixes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,32 @@
-import BattleManager from "./battleManager";
+class BattleManager {
+    /** Enter damage to deal to the enemy as an argument to the method. */
+    static damageEnemy(damage, enemyHealth) {
+        enemyHealth = enemyHealth - damage;
+
+        // Log a resulting message to the console so we know that it worked.
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+    }
+
+    /** Enter damage to deal to the player as an argument to the method. */
+    static damagePlayer(damage, playerHealth) {
+        // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+        playerHealth = playerHealth - damage;
+
+        // Log a resulting message to the console so we know that it worked.
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+    }
+}
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -24,4 +52,5 @@ var fight = function () {
 
 fight();
 
+//Helper Class
 
