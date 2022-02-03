e7ca56d8158e736236fb994546ba03189913e827
jonmanquincy <53950487+jonmanquincy@users.noreply.github.com>
2022-02-03 13:28:40 -0700

    gave in and followed module

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,95 +10,80 @@ var enemyAttack = 12;
 
 
 
-class BattleManager {
-    static runGame(enemyNames) {
-        // Alert players that they are starting the round
-        window.alert("Welcome to Robot Gladiators!");
+var fight = function (enemyName) {
+    while (playerHealth > 0 && enemyHealth > 0) {
+        // ask player if they'd like to fight or run
+        var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-        enemyNames.forEach(name => {
-            this.fight(name);
-        });
+        // if player picks "skip" confirm and then stop the loop
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            // confirm player wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-    }
-
-    //Combat
-    static fight(enemyName) {
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
-        //Resisting just making an enum
-        if (promptFight) {promptFight = promptFight.toLowerCase();}
-
-        switch (promptFight) {
-            case "fight":
-                //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-                BattleManager.damageEnemy(playerAttack, enemyHealth, enemyName);
-
-                // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-                BattleManager.damagePlayer(enemyAttack, playerHealth, enemyName);
+            // if yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + ' has decided to skip this fight. Goodbye!');
+                // subtract money from playerMoney for skipping
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney)
                 break;
-
-            case "skip":
-                BattleManager.skip(enemyName);
-                break;
-
-            default:
-                window.alert("You need to choose a valid option. Try again!");
+            }
         }
-    }
 
-    /** Enter damage to deal to the enemy as an argument to the method. */
-    static damageEnemy(damage, enemyHealth, enemyName) {
-        enemyHealth = enemyHealth - damage;
-
-        // Log a resulting message to the console so we know that it worked.
+        // remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
         console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
 
+        // check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
-        }
-        else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
-        }
-    }
+            window.alert(enemyName + ' has died!');
 
-    /** Enter damage to deal to the player as an argument to the method. */
-    static damagePlayer(damage, playerHealth, enemyName) {
-        // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
-        playerHealth = playerHealth - damage;
+            // award player money for winning
+            playerMoney = playerMoney + 20;
 
-        // Log a resulting message to the console so we know that it worked.
+            // leave while() loop since enemy is dead
+            break;
+        } else {
+            window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
+        }
+
+        // remove players's health by subtracting the amount set in the enemyAttack variable
+        playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
 
         // check player's health
         if (playerHealth <= 0) {
-            window.alert(playerName + " has died!");
-        }
-        else {
-            window.alert(playerName + " still has " + playerHealth + " health left.");
+            window.alert(playerName + ' has died!');
+            // leave while() loop if player is dead
+            break;
+        } else {
+            window.alert(playerName + ' still has ' + playerHealth + ' health left.');
         }
     }
+};
 
-    static skip(enemyName) {
-        // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-        // if yes (true), leave fight
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 2;
-        }
-        // if no (false), ask question again by running fight() again
-        else {
-            this.fight(enemyName);
-        }
+
+for (let index = 0; index < enemyNames.length; index++) {
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (index + 1));
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[index];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
     }
 }
-
-
-
-BattleManager.runGame(enemyNames);
