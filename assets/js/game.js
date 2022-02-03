cc951a786eb8d8c7e4c88bc6dd3e3b7310beb196
jonmanquincy <53950487+jonmanquincy@users.noreply.github.com>
2022-02-03 12:27:05 -0700

    switch added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,40 @@
+//JavaScript is weird. Tried seperating this into it's own file but turns out if you aren't running this on a server it wont let you modulate -_-
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
 class BattleManager {
+    static runGame() {
+        // Alert players that they are starting the round
+        window.alert("Welcome to Robot Gladiators!");
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+        //Resisting just making an enum
+        promptFight = promptFight.toLowerCase();
+        switch (promptFight) {
+
+            case "fight":
+                //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+                BattleManager.damageEnemy(playerAttack, enemyHealth);
+
+                // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+                BattleManager.damagePlayer(enemyAttack, playerHealth);
+                break;
+
+            case "skip":
+                BattleManager.skip();
+                break;
+
+            default:
+                window.alert("You need to choose a valid option. Try again!");
+        }
+    }
     /** Enter damage to deal to the enemy as an argument to the method. */
     static damageEnemy(damage, enemyHealth) {
         enemyHealth = enemyHealth - damage;
@@ -25,32 +61,33 @@ class BattleManager {
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
+
+        // check player's health
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+    }
+
+    static skip() {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // if yes (true), leave fight
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            // subtract money from playerMoney for skipping
+            playerMoney = playerMoney - 2;
+        }
+        // if no (false), ask question again by running fight() again
+        else {
+            fight();
+        }
     }
 }
 
-var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
-var playerAttack = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
-var enemyHealth = 50;
-var enemyAttack = 12;
-
-var fight = function () {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    BattleManager.damageEnemy(playerAttack, enemyHealth);
-
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    BattleManager.damagePlayer(enemyAttack, playerHealth);
-};
-
-fight();
-
-//Helper Class
-
+BattleManager.runGame();
